bef7687aadf286c7e387026f0e49b250c4586126
Shrish1239 <160769232+Shrish1239@users.noreply.github.com>
2024-03-30 10:52:00 +0530

    Update start.py

--- STORM/modules/start.py
+++ STORM/modules/start.py
@@ -3,17 +3,17 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_OP = [
     [
-        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/Kexx_XD"),
-        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/STORM_CHATZ"),
+        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/Homosapienhu"),
+        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/sudeokeliyeaajaobclog"),
     ],
     [
         Button.url(
-            "ɢʀᴏᴜᴘ 🧸", "https://t.me/FriendCastel"
+            "ɢʀᴏᴜᴘ 🧸", "https://t.me/sudeokeliyeaajaobclog"
         ),
     ],
     [
-        Button.url("ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ❄️", "https://github.com/VARC9210/STORM"),
-        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/STORM_TECHH"),
+        Button.url("ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ❄️", "https://graph.org/file/278070bbdbfb0a29f0d8a.mp4"),
+        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/sudeokeliyeaajaobclog"),
     ],
 ]
 
@@ -35,13 +35,13 @@ async def start(event):
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ 🥀{event.sender.first_name}❤️**\n\n**ɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n"
         TEXT += f"➖➖➖➖➖➖➖➖➖➖➖\n"
-        TEXT += f"» **ꜱᴇɴꜱᴇɪ 🫂: [⏤͟͞〲ᴅᴇᴠɪʟ](https://t.me/KANU_XD)**\n"
-        TEXT += f"» **ꜱᴛᴏʀᴍ ⚙️:** `3.0` \n"
+        TEXT += f"» **ꜱᴇɴꜱᴇɪ 🫂: [𝚆𝙰𝙽𝙳𝙴𝚁𝙸𝙽𝙶](https://t.me/Homosapienhu)**\n"
+        TEXT += f"» **𝚆𝙰𝙽𝙳𝙴𝚁𝙸𝙽𝙶 𝚇 𝚂𝙿𝙰𝙼𝙱𝙾𝚃 ⚙️:** `3.0` \n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ 🐍:** `3.11` \n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ 🔰:** `{event.client.__version__}`\n➖➖➖➖➖➖➖➖➖➖➖"
         await event.client.send_file(
                     event.chat_id,  
-                    "https://graph.org/file/9b43d66ff422d98f6b56e.jpg",
+                    "https://graph.org/file/5b8541384225ee73a40ac.jpg",
                     caption=TEXT, 
                     buttons=START_OP
                 )
